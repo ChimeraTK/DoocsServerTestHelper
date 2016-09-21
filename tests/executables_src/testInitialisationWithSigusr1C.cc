009f15d7b60b276f91b92d1c803cf4b5ac47f240
Martin Hierholzer <martin.hierholzer@desy.de>
2016-09-21 15:57:47 +0200

    added another test for sigusr1

---
+++ tests/executables_src/testInitialisationWithSigusr1C.cc
@@ -0,0 +1,40 @@
+#include "testDoocsServerTestHelper_skeleton.h"
+
+void HelperTest::testRoutineBody() {
+
+  // start the initialisation in the background. it cannot complete since we do not yet enter nanosleep in the update thread)
+  std::thread t([](){
+    std::cout << "DoocsServerTestHelper::initialise() ->" << std::endl;
+    DoocsServerTestHelper::initialise();
+    std::cout << "<- DoocsServerTestHelper::initialise()" << std::endl;
+  });
+
+  // wait some time, then enter nanosleep in the update thread
+  usleep(100000);
+  allowUpdate();
+
+  // initialise will execute update once, so wait until nanosleep has finished the first time
+  waitUpdate();
+
+  // now let the update thread enter nanosleep another time, so initialise() can complete
+  allowUpdate();
+
+  // now the initialisation must be able to complete
+  t.join();
+
+  // test a full update cycle
+  allowUpdate();
+  std::cout << "DoocsServerTestHelper::runUpdate() ->" << std::endl;
+  DoocsServerTestHelper::runUpdate();
+  std::cout << "<- DoocsServerTestHelper::runUpdate()" << std::endl;
+  waitUpdate();
+
+  // test a full sigusr1 cycle
+  allowSigusr1();
+  allowSigusr1();   // twice since the tread needs to enter sigwait() again before runSigusr1() returns
+  std::cout << "DoocsServerTestHelper::runSigusr1() ->" << std::endl;
+  DoocsServerTestHelper::runSigusr1();
+  std::cout << "<- DoocsServerTestHelper::runSigusr1()" << std::endl;
+  waitSigusr1();
+
+}
