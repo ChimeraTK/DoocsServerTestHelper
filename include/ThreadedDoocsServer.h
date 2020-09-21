128fe46e8f3fa51b17c7ceff5e629adf2aa93be1
Martin Hierholzer <martin.hierholzer@desy.de>
2020-09-21 09:42:26 +0200

    randomnize BPNs for tests (fixes issues with ZeroMQ tests)
    see ChimeraTK/DeviceAccess-DoocsBackend#44

--- include/ThreadedDoocsServer.h
+++ include/ThreadedDoocsServer.h
@@ -20,8 +20,9 @@ class ThreadedDoocsServer {
     if(not _configFile.empty()) {
       auto pos = _configFile.find(".conf");
       _serverName = _configFile.substr(0, pos);
-      // update config file with the RPC number
-      std::string command = "sed -i " + _configFile + " -e 's/^SVR.RPC_NUMBER:.*$/SVR.RPC_NUMBER: " + rpcNo() + "/'";
+      // update config file with the RPC number and BPN
+      std::string command = "sed -i " + _configFile + " -e 's/^SVR.RPC_NUMBER:.*$/SVR.RPC_NUMBER: " + rpcNo() +
+          "/' -e 's/^SVR.BPN:.*$/SVR.BPN: " + bpn() + "/'";
       auto rc = std::system(command.c_str());
       (void)rc;
     }
@@ -50,9 +51,20 @@ class ThreadedDoocsServer {
     return _rpcNo;
   }
 
+  std::string bpn() {
+    if(_bpn.empty()) {
+      std::random_device rd;
+      std::uniform_int_distribution<int> dist(200, 6500);
+      // range is 2000 to 65000 in steps of 10 (BPNs allocate a block of port numbers used for ZeroMQ)
+      _bpn = std::to_string(dist(rd)) + "0";
+    }
+
+    return _bpn;
+  }
+
  protected:
   std::string _serverName;
-  std::string _rpcNo;
+  std::string _rpcNo, _bpn;
   std::string _configFile;
   std::thread _doocsServerThread;
 };
